2a9779203dd7879a3104e6858a0ad53a8b90a5d0
gartral <gareth7yo@gmail.com>
2021-04-13 17:43:39 -0400

    modified:   ed_keypress.sh

--- ed_keypress.sh
+++ ed_keypress.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-passh -p YOUR_USER_PASSWORD ssh USER@IP-ADDRESS /bin/bash -c "'DISPLAY=:0 xdotool keydown $1; sleep 12; xdotool keyup $1'"
+#passh -p YOUR_USER_PASSWORD 
+ssh -f -o ServerAliveInterval=6000 USER@IP-ADDRESS /bin/bash -c "'DISPLAY=:0 xdotool keydown $1; sleep 12; xdotool keyup $1'"
